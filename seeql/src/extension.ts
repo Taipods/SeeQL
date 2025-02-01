a2ef06c0264f4272f37eb586f868c9f02351ee8a
Yeli.James <jbmalvar.cs.washington.edu>
2025-02-01 05:01:31 -0800

    First Command Block Added - Create Diagram

--- seeql/src/extension.ts
+++ seeql/src/extension.ts
@@ -13,13 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('seeql.helloWorld', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('seeql.createDiagram', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SeeQL!');
-	});
-
-	context.subscriptions.push(disposable);
+		vscode.window.showInformationMessage('Create a Diagram of a Create SQL Statement');
+	}));
 }
 
 // This method is called when your extension is deactivated
